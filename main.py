25ca077c296a04d44e54c04d0a3a9a46a1d9ecec
HenriqueRBomfim <henriquerb1@al.insper.edu.br>
2022-05-10 17:57:28 -0300

    Comentei as partes do programa fiz o print do começo do jogo

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
+#Importação das bibliotecas
 import math
 import random
 import funcoes_henrique as fh
 import Funcoes_eduardo_selber as fe
 import BaseDePaises as base
 
+#Declaração de Variáveis
 base_normalizada = fh.normaliza(base.DADOS)
+
+#Início do Jogo
+print(" ============================\n|                            |\n| Bem-vindo ao Insper Países |\n|                            |")
+print(" ==== Design de Software ==== \n\n Comandos:\n\n    dica       - entra no mercado de dicas\n    desisto    - desiste da rodada")
+print("    inventario - exibe sua posição\n\nUm país foi escolhido, tente adivinhar!")
+
+#Jogo
