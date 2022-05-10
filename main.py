0d0ada459aba8e598b2cda6e60a061ba7c046381
HenriqueRBomfim <henriquerb1@al.insper.edu.br>
2022-05-10 18:14:15 -0300

    Comecei o loop

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-#Importação das bibliotecas
-import math
-import random
+#import math
+#import random
 import funcoes_henrique as fh
 import Funcoes_eduardo_selber as fe
 import BaseDePaises as base
 
 #Declaração de Variáveis
 base_normalizada = fh.normaliza(base.DADOS)
+tentativas = 20
 
 #Início do Jogo
 print(" ============================\n|                            |\n| Bem-vindo ao Insper Países |\n|                            |")
@@ -14,3 +14,8 @@ print(" ==== Design de Software ==== \n\n Comandos:\n\n    dica       - entra no
 print("    inventario - exibe sua posição\n\nUm país foi escolhido, tente adivinhar!")
 
 #Jogo
+
+#while tentativas > 0:
+    #print("Você tem {0} tentativa(s)".format(tentativas))
+    #palpite = input("Qual seu palpite? ")
+    #tentativas -= 1
