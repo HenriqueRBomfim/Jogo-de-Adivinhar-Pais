698c017df466f339d99c5b5382932a9b08fca2d6
HenriqueRBomfim <henriquerb1@al.insper.edu.br>
2022-05-10 18:35:33 -0300

    adicionei o mercado de dicas, o que ocorre se o usuário digitar desisto e a geração aleatória de um país

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-#import math
-#import random
+#Importação de bibliotecas
+import math
+import random
 import funcoes_henrique as fh
 import Funcoes_eduardo_selber as fe
 import BaseDePaises as base
@@ -13,9 +14,28 @@ print(" ============================\n|                            |\n| Bem-vind
 print(" ==== Design de Software ==== \n\n Comandos:\n\n    dica       - entra no mercado de dicas\n    desisto    - desiste da rodada")
 print("    inventario - exibe sua posição\n\nUm país foi escolhido, tente adivinhar!")
 
+#Sorteando o país
+resposta = fh.sorteia_pais(base_normalizada)
+
 #Jogo
 
-#while tentativas > 0:
-    #print("Você tem {0} tentativa(s)".format(tentativas))
-    #palpite = input("Qual seu palpite? ")
-    #tentativas -= 1
+while tentativas > 0:
+    print("Você tem {0} tentativa(s)".format(tentativas))
+    palpite = input("Qual seu palpite? ")
+    if palpite == 'dica':
+        print('''Mercado de Dicas
+----------------------------------------
+1. Cor da bandeira  - custa 4 tentativas
+2. Letra da capital - custa 3 tentativas
+3. Área             - custa 6 tentativas
+4. População        - custa 5 tentativas
+5. Continente       - custa 7 tentativas
+0. Sem dica
+----------------------------------------''')
+        opcao = int(input("Escolha sua opção [0|1|2|3|4|5]: "))
+    if palpite == 'desisto':
+        tentativas = 0
+        print(resposta)
+    if palpite == 'inventario':
+        print("Inventário: ")
+    tentativas -= 1
