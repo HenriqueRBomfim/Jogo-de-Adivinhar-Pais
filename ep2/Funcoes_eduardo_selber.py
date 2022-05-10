93d6dfb96ab02e2e9e3191df6434289767bad71d
Selber_codes <eduardosc6@al.insper.edu.br>
2022-05-10 13:11:28 -0300

    Funções de Eduardo Selber

---
+++ ep2/Funcoes_eduardo_selber.py
@@ -0,0 +1,33 @@
+import random 
+def sorteia_letra(plv,res):
+    letras=[]
+    c=['.', ',', '-', ';', ' ']
+    for i in plv:
+        if i.lower() not in c and i.lower() not in res:
+            letras.append(i.lower())
+    if len(letras)==0:
+        return ''
+    
+    return random.choice(letras)
+def esta_na_lista(nome,lista):
+    for i in lista:
+        if i[0]==nome:
+            return True
+    return False
+def adiciona_em_ordem(nome,distancia,lista):
+    combinado=[nome,distancia]
+    c=0
+    if len(lista)==0:
+        lista.append(combinado)
+        return lista
+    for i in lista:
+
+        if i[1]>distancia and i[0] not in lista:
+            lista.insert(c,combinado)
+            return lista 
+
+        c+=1
+    if nome not in lista:
+        lista.append(combinado) 
+        return lista
+    return lista
