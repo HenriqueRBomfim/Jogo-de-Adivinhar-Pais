6bf3e76f68b01769ca5ee0a6927078e9c4d10d75
Selber_codes <eduardosc6@al.insper.edu.br>
2022-05-11 14:28:15 -0300

    inventario criado, dica invalida e repete a pergunta

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ def jogo(num):
     #Declarando variáveis
     tentativas = 20
     conta_dica_3, conta_dica_4, conta_dica_5 = 0,0,0
-    arcetou=0
+    acertou=0
     letras = []
     lista_cores = []
     lista_cores_sorteadas = []
@@ -34,6 +34,8 @@ def jogo(num):
     contine = 0
     ordem_paises_print=''
     ordenando=0
+    dica_invalida=0
+    lista_opcoes_dicas=[0,1,2,3,4,5]
     mercado_dicas='''Mercado de Dicas
     ----------------------------------------
     \n1. Cor da bandeira  - custa 4 tentativas\n2. Letra da capital - custa 3 tentativas\n3. Área             - custa 6 tentativas\n4. População        - custa 5 tentativas\n5. Continente       - custa 7 tentativas
@@ -63,65 +65,77 @@ def jogo(num):
         palpite = input("Qual seu palpite? ")
         if palpite == 'dica':
             print(mercado_dicas)
-            opcao = int(input(opcoes_dica))
-            if opcao == 1:
-                    tentativas -= 4
-                    cor_sorteada = random.choice(lista_cores)
-                    lista_cores.remove(cor_sorteada)
-                    lista_cores_sorteadas.append(cor_sorteada)
-                    dicas_print += '-Cores da bandeira:{}\n'.format(lista_cores_sorteadas)
-                    tentativas+=1
-            if opcao == 2:
-                    tentativas -= 3
-                    sorteada = fe.sorteia_letra(resposta, letras)
-                    letras.append(sorteada)
-                    dicas_print += '-Letras da capital:{}\n'.format(letras)
-                    tentativas+=1
-            if opcao == 3:
-                if conta_dica_3 > 0:
-                    print("Opção inválida")
-                    tentativas+=1
-                else:
-                    conta_dica_3 = 1
-                    tentativas -= 6
-                    a = base_normalizada[resposta]['area']
-                    dicas_print += '-Área:{}km²\n'.format(a)
-                    opcoes_dica_atualizado =opcoes_dica.replace('|3','')
-                    mercado_dica_atualizado=mercado_dicas.replace('3. Área             - custa 6 tentativas\n','')
-                    mercado_dicas=mercado_dica_atualizado
-                    opcoes_dica=opcoes_dica_atualizado
-                    tentativas+=1
-            if opcao == 4:
-                if conta_dica_4 > 0:
-                    print("Opção inválida")
-                    tentativas+=1
-                else:
-                    conta_dica_4 = 1
-                    tentativas -= 5
-                    popula = base_normalizada[resposta]['populacao']
-                    dicas_print += 'População:{}\n'.format(popula)
-                    opcoes_dica_atualizado=opcoes_dica.replace('|4','')
-                    mercado_dica_atualizado=mercado_dicas.replace(    '4. População        - custa 5 tentativas\n','')
-                    mercado_dicas=mercado_dica_atualizado
-                    opcoes_dica=opcoes_dica_atualizado
-                    tentativas+=1
-            if opcao == 5:
-                if conta_dica_5 > 0:
-                    print("Opção inválida")
-                    tentativas+=1
-                else:
-                    contine = base_normalizada[resposta]["continente"]
-                    conta_dica_5 = 1
-                    tentativas -= 7
-                    dicas_print += '-Continente:{}\n'.format(contine)
-                    opcoes_dica_atualizado=opcoes_dica.replace('|5','')
-                    mercado_dica_atualizado=mercado_dicas.replace('5. Continente       - custa 7 tentativas\n','')
-                    mercado_dicas=mercado_dica_atualizado
-                    opcoes_dica=opcoes_dica_atualizado
-                    tentativas+=1                   
-            if opcao == 0:
-                tentativas += 1
-            print('''Dicas:\n{}\nDistancias:\n{}'''.format(dicas_print,ordem_paises_print))    
+            opcao = str(input(opcoes_dica))
+            while dica_invalida!=1:
+                if opcao == '1':
+                        tentativas -= 4
+                        cor_sorteada = random.choice(lista_cores)
+                        lista_cores.remove(cor_sorteada)
+                        lista_cores_sorteadas.append(cor_sorteada)
+                        dicas_print += '-Cores da bandeira:{}\n'.format(lista_cores_sorteadas)
+                        tentativas+=1
+                        dica_invalida=1
+                if opcao == '2':
+                        tentativas -= 3
+                        sorteada = fe.sorteia_letra(resposta, letras)
+                        letras.append(sorteada)
+                        dicas_print += '-Letras da capital:{}\n'.format(letras)
+                        tentativas+=1
+                        dica_invalida=1
+                if opcao == '3':
+                    if conta_dica_3 > 0:
+                        print("Opção inválida")
+                        tentativas+=1
+                    else:
+                        conta_dica_3 = 1
+                        tentativas -= 6
+                        a = base_normalizada[resposta]['area']
+                        dicas_print += '-Área:{}km²\n'.format(a)
+                        opcoes_dica_atualizado =opcoes_dica.replace('|3','')
+                        mercado_dica_atualizado=mercado_dicas.replace('3. Área             - custa 6 tentativas\n','')
+                        mercado_dicas=mercado_dica_atualizado
+                        opcoes_dica=opcoes_dica_atualizado
+                        tentativas+=1
+                        dica_invalida=1
+                if opcao == '4':
+                    if conta_dica_4 > 0:
+                        print("Opção inválida")
+                        tentativas+=1
+                    else:
+                        conta_dica_4 = 1
+                        tentativas -= 5
+                        popula = base_normalizada[resposta]['populacao']
+                        dicas_print += 'População:{}\n'.format(popula)
+                        opcoes_dica_atualizado=opcoes_dica.replace('|4','')
+                        mercado_dica_atualizado=mercado_dicas.replace(    '4. População        - custa 5 tentativas\n','')
+                        mercado_dicas=mercado_dica_atualizado
+                        opcoes_dica=opcoes_dica_atualizado
+                        tentativas+=1
+                        dica_invalida=1
+                if opcao == '5':
+                    if conta_dica_5 > 0:
+                        print("Opção inválida")
+                        tentativas+=1
+                    else:
+                        contine = base_normalizada[resposta]["continente"]
+                        conta_dica_5 = 1
+                        tentativas -= 7
+                        dicas_print += '-Continente:{}\n'.format(contine)
+                        opcoes_dica_atualizado=opcoes_dica.replace('|5','')
+                        mercado_dica_atualizado=mercado_dicas.replace('5. Continente       - custa 7 tentativas\n','')
+                        mercado_dicas=mercado_dica_atualizado
+                        opcoes_dica=opcoes_dica_atualizado
+                        tentativas+=1  
+                        dica_invalida=1                 
+                if opcao == '0':
+                    tentativas += 1
+                    dica_invalida=1
+                if opcao!='0' and opcao!='1' and opcao!='2' and opcao!='3' and opcao!='4' and opcao!='5':
+                    print('Opção inválida')
+                    opcao = str(input(opcoes_dica))
+                    dica_invalida=0
+        dica_invalida=0
+        print('''Dicas:\n{}\nDistancias:\n{}'''.format(dicas_print,ordem_paises_print))    
 
         if palpite == 'desisto':
             confirmacao = input("Tem certeza que deseja desistir da rodada? [s|n] ")
@@ -130,11 +144,11 @@ def jogo(num):
                 tentativas = 0
 
         if palpite == 'inventario':
-            print("Distâncias:")
-            #print()
+            print('''Dicas:\n{}\nDistancias:\n{}'''.format(dicas_print,ordem_paises_print)) 
+            tentativas+=1  
 
         verificando_na_lista = fe.esta_na_lista(palpite,lista_tds_paises)
-        if palpite.lower() not in lista_tds_paises and palpite != 'desisto' and palpite!='dica':
+        if palpite.lower() not in lista_tds_paises and palpite != 'desisto' and palpite!='dica' and palpite!='inventario':
             print('País invalido')
             tentativas+=1
         if palpite.lower() in lista_tds_paises:
